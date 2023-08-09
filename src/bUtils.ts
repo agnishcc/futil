99f6dbe9b3d8a1cb435c3e658fc681e445136290
Agnish Chakraborty <agnishc.dev@gmail.com>
2023-08-10 00:16:36 +0530

    feat:sparkles: : Adds backend utilities class

---
+++ src/bUtils.ts
@@ -0,0 +1,32 @@
+interface IBUtils {
+    convertArrToHasuraObj(arr: any[]): string;
+}
+
+/**
+ * @description Utility functions for the backend
+ * @class BUtils
+ * @implements {IBUtils}
+ * @example const bUtils = new BUtils();
+ */
+class BUtils implements IBUtils {
+   
+    /**
+     * @description Converts an array to a Hasura object
+     * @param {any[]} arr
+     * @returns {string}
+     * @memberof BUtils
+     * @example const hasuraObj = bUtils.convertArrToHasuraObj(['id', 'name']); // {id, name}
+     * @example const hasuraObj = bUtils.convertArrToHasuraObj(['id']); // {id}
+     * @example const hasuraObj = bUtils.convertArrToHasuraObj([]); // {}
+     */
+   public convertArrToHasuraObj(arr: any[]): string {
+        let str = '{';
+        arr.forEach((item) => {
+            str += item + ',';
+        });
+        str = str.slice(0, -1);
+        str += '}';
+        return str;
+    }
+}
+
--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,32 @@ import { Utils } from "./utils";
 
 /**
  * @description
- * This is the entry point of the library.
+ * Utils for working with various random things.
  * 
  */
 
 export const utils = new Utils();
+
+/**
+ * @description
+ * Utils for working with types of  strings.
+ * 
+ */
 export const strings = new Strings();
+
+/**
+ * @description
+ * Utils working with backend.
+ */
+export const bUtils = new BUtils();
+
+/**
+ * @description
+ * This is the entry point of the library.
+ */
 export const futil = {
     utils,
-    strings
+    strings,
+    bUtils
 }
 
